5b0f0fa428d5fb465da768ab74ff00c879dd58fe
tienthienhd <tienthienhd@gmail.com>
2023-07-12 17:24:36 +0700

    buid docker

--- build_docker.sh
+++ build_docker.sh
@@ -1,3 +1,3 @@
-docker build --platform=linux/amd64 -t tienthienhd/index_color:3.0.0 .
-docker push tienthienhd/index_color:3.0.0
-docker run --rm --name test -p 10001:10001 tienthienhd/index_color:3.0.0 
+docker build --platform=linux/amd64 -t tienthienhd/index_color:3.0.1 .
+docker push tienthienhd/index_color:3.0.1
+docker run --rm --name test -p 10001:10001 tienthienhd/index_color:3.0.1
